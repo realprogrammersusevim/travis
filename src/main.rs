a7c42072c80448fdffadfdde6dcd8d60629de58a
Jonathan <git@jonathansm.dev>
2023-03-13 01:06:03 -0500

    fix: Print everything on one line
    Instead of printing each iteration on a separate line each predicted
    word is appended to the end.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use rand::{thread_rng, Rng};
 use rayon::prelude::*;
 use std::env;
 use std::fs::read_to_string;
+use std::io::Write;
 
 fn main() {
     let args = Args::parse();
@@ -22,25 +23,26 @@ fn main() {
     let file = read_to_string(args.file).unwrap();
     info!("File read");
     info!("Splitting file");
-    let mut words = Vec::new();
+    let mut words: Vec<String> = Vec::new();
     for line in file.split('\n') {
         for word in line.split(' ') {
-            words.push(word)
+            words.push(word.replace("\r", ""))
         }
     }
     info!("File split");
 
     info!("Finding words");
+    let placeholder = &String::from("");
     let mut sentence: Vec<String> = vec![input.to_string()];
     for i in 0..args.length {
         let found: &Vec<_> = &words
             .par_iter()
             .enumerate()
             .filter(|(_, word)| *word == &sentence[i].clone())
-            .map(|(index, _)| words.get(index + 1).unwrap_or(&""))
+            .map(|(index, _)| words.get(index + 1).unwrap_or(placeholder))
             .collect();
 
-        info!("{found:?}");
+        // info!("{found:?}");
 
         let random_indx = thread_rng().gen_range(0..found.len());
         let next = found[random_indx].to_string();
@@ -49,8 +51,12 @@ fn main() {
 
         sentence.push(next);
 
-        println!("{}", sentence.join(" "));
+        info!("{sentence:?}");
+
+        print!("\r{}", sentence.join(" "));
+        std::io::stdout().flush().unwrap();
     }
+    println!()
 }
 
 #[derive(Parser, Debug)]
